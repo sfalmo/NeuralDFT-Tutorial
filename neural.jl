3826a66e81f84982960e71d2e5e869484ffb3f65
Florian Sammüller <florian.sammueller@uni-bayreuth.de>
2023-10-07 17:18:13 +0200

    Add further ML text

--- neural.jl
+++ neural.jl
@@ -15,19 +15,28 @@ function read_sim_data(dir)
     ρ_profiles, c1_profiles
 end
 
-function generate_inout(ρ_profiles, c1_profiles; window_bins=201)
-    ρ_windows = Vector{Vector{Float64}}()
-    c1_values = Vector{Float64}()
+function generate_windows(ρ; window_bins=201)
+    ρ_windows = zeros(window_bins, length(ρ))
     pad = window_bins ÷ 2 - 1
+    ρpad = vcat(ρ[end-pad:end], ρ, ρ[begin:begin+pad])
+    for i in 1:length(ρ)
+        ρ_windows[:,i] = ρpad[i:i+window_bins-1]
+    end
+    ρ_windows
+end
+
+function generate_inout(ρ_profiles, c1_profiles; window_bins=201)
+    ρ_windows_all = Vector{Vector{Float64}}()
+    c1_values_all = Vector{Float64}()
     for (ρ, c1) in zip(ρ_profiles, c1_profiles)
-        ρpad = vcat(ρ[end-pad:end], ρ, ρ[begin:begin+pad])
+        ρ_windows = generate_windows(ρ; window_bins)
         for i in 1:length(c1)
             if isnan(c1[i])
                 continue
             end
-            push!(ρ_windows, ρpad[i:i+window_bins-1])
-            push!(c1_values, c1[i])
+            push!(ρ_windows_all, ρ_windows[:,i])
+            push!(c1_values_all, c1[i])
         end
     end
-    Float32.(hcat(ρ_windows...)), Float32.(c1_values')
+    Float32.(hcat(ρ_windows_all...)), Float32.(c1_values_all')
 end
