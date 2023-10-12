045fe829e887843630168c8cd103f467eab12bc5
Florian Sammüller <florian.sammueller@uni-bayreuth.de>
2023-10-12 12:40:07 +0200

    Fix Percus weights and typos, add sedimentation

--- dft.jl
+++ dft.jl
@@ -9,11 +9,14 @@ function get_weights_Percus(xs)
     dx = xs[2]
     L = xs[end] + dx
     R = 0.5
+    @assert L >= 2*R "To construct the Percus weight functions, the system must have a length of at least 2R."
+    @assert count(xs .≈ R) == 1 "The grid is not suitable for the construction of the Percus weight functions. R must be a multiple of dx."
     ω0 = zero(xs)
-    @assert count(xs .≈ R .|| xs .≈ L - R) == 2 "The grid is not suitable for the construction of the Percus weight functions. R must be a multiple of dx."
-    ω0[xs.≈R.||xs.≈L-R] .= 0.5 / dx
+    ω0[xs.≈R] .= 0.5 / dx
+    ω0[2:end] += reverse(ω0[2:end])
     ω1 = zero(xs)
-    ω1[xs.<R.||xs.>L-R] .= 1
-    ω1[xs.≈R.||xs.≈L-R] .= 0.5
+    ω1[xs.<R] .= 1
+    ω1[xs.≈R] .= 0.5
+    ω1[2:end] += reverse(ω1[2:end])
     ω0, ω1
 end
